a411fc12ff1edad003f785e41c6dadcf84b0809c
Dmytro Nykorovych <dev.dmytronykorovych@gmail.com>
2025-08-05 12:50:23 +0200

    Task solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,56 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  let day = '';
+  let month = '';
+  let year = '';
+  const result = [];
+
+  const fromSeparator = fromFormat[fromFormat.length - 1];
+  const toSeparator = toFormat[toFormat.length - 1];
+  const dateArr = date.split(fromSeparator);
+
+  for (let i = 0; i < dateArr.length; i++) {
+    if (fromFormat[i] === 'DD') {
+      day = dateArr[i];
+    }
+
+    if (fromFormat[i] === 'MM') {
+      month = dateArr[i];
+    }
+
+    if (fromFormat[i] === 'YYYY') {
+      year = dateArr[i];
+    }
+
+    if (fromFormat[i] === 'YY' && toFormat.includes('YYYY')) {
+      if (dateArr[i] < 30) {
+        year = `20${dateArr[i]}`;
+      } else {
+        year = `19${dateArr[i]}`;
+      }
+    }
+  }
+
+  for (let i = 0; i < dateArr.length; i++) {
+    if (toFormat[i] === 'DD') {
+      result[i] = day;
+    }
+
+    if (toFormat[i] === 'MM') {
+      result[i] = month;
+    }
+
+    if (toFormat[i] === 'YYYY') {
+      result[i] = year;
+    }
+
+    if (toFormat[i] === 'YY') {
+      result[i] = year.slice(-2);
+    }
+  }
+
+  return result.join(toSeparator);
 }
 
 module.exports = formatDate;
